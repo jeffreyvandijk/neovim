b03363a62a5395ae6ad0d40882c20fa88299393c
Jeffrey Van Dijk <jvdijk90@gmail.com>
2025-07-18 22:16:45 +0200

    treesitter to plugins

--- init.lua
+++ init.lua
@@ -27,11 +27,4 @@ require("lazy").setup("plugins")
 
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua"},
-  highlight = { enable = true },
-  indent = { enable = true },
-})	
-
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,11 +1,6 @@
 return {
   
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
