d2a4095afb9c79995ae14bff740a87ed68b5cd5b
Jeffrey Van Dijk <jvdijk90@gmail.com>
2025-07-21 21:33:38 +0200

    added completions / debugging

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,32 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "mfussenegger/nvim-dap-python",
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+
+    local dap = require("dap")
+    local dapui = require("dapui")
+    require("dap-python").setup("~/.config/nvim/python_test/venv/bin/python")
+    require("dapui").setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+  end,
+}
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,12 +16,17 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ansiblels.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ansiblels.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gr', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end
-     }
+  }
 }
