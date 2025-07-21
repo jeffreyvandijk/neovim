0fcaeb9b2409e00fb4613290f283a1aa1d5df91b
Jeffrey Van Dijk <jvdijk90@gmail.com>
2025-07-21 14:54:52 +0200

    linters & dashboard

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,8 +20,8 @@ return {
       lspconfig.lua_ls.setup({})
       lspconfig.ansiblels.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end
      }
 }
